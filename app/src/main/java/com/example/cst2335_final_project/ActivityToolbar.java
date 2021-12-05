41a2e301a549d5b02ec95057ae67f97247ebc9a5
Mimi Dib <dib00016@algonquinlive.com>
2021-12-05 22:57:40 +0500

    Milestone 2 in progress...
    -activity_main.xml toolbar <include> runtime error

---
+++ app/src/main/java/com/example/cst2335_final_project/ActivityToolbar.java
@@ -0,0 +1,14 @@
+package com.example.cst2335_final_project;
+
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class ActivityToolbar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
+    }
+}
--- app/src/main/java/com/example/cst2335_final_project/MainActivity.java
+++ app/src/main/java/com/example/cst2335_final_project/MainActivity.java
@@ -1,29 +1,57 @@
 package com.example.cst2335_final_project;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     EditText editDate;
     Button submit, tempButton;
+    Toolbar toolbar;
     final Calendar myCalendar = Calendar.getInstance();
+    public static final String ACTIVITY_NAME = "MAIN_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); //loads toolbar -> also calls onCreateOptionsMenu if we need one l8er
+        //nav drawer
+        DrawerLayout drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggleDrawer = new ActionBarDrawerToggle(this, drawer,
+                toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggleDrawer);
+        toggleDrawer.syncState();
+        //Navigation View -> from activity_toolbar > navbar contents
+        NavigationView navView = findViewById(R.id.navView);
+        navView.setNavigationItemSelectedListener(this);
 
         //temporary button to go to favourites -> drawer will take to favourites later
         tempButton = (Button) findViewById(R.id.tempButton);
@@ -59,11 +87,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //go to activity_generate.xml with date data from editText
-                //TODO [Submit] test if mainactivity.this works or just .this
                 Intent imageIntent = new Intent(MainActivity.this, ActivityImageGen.class);
                 startActivity(imageIntent);
             }
         });
+        //log
+        Log.e(ACTIVITY_NAME, "*****Inside OnCreate()*****");
+    }
+    //TODO toolbar options menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate menu?
+        return super.onCreateOptionsMenu(menu);
     }
 
     //update label method
@@ -73,11 +108,24 @@ public class MainActivity extends AppCompatActivity {
 
         editDate.setText(simpleDateFormat.format(myCalendar.getTime()));
     }
-    //favourites activity
-    public void openFavouritesActivity(){
-        //TODO [openFavActivity] test if mainactivity.this works or just .this
-        Intent favIntent = new Intent(MainActivity.this, favouritesActivity.class);
-        startActivity(favIntent);
-    }
 
+    //Milestone 2 -> toolbar
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        /*
+        //switch case -> action on item selected
+        switch(item.getItemId()){
+            case R.id.navHome: //TODO : is there a way to just close the drawer ?
+                Toast.makeText(this, "Already on Home page", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navFav: //open favouritesActivity
+                Intent favIntent = new Intent(this, favouritesActivity.class);
+                startActivity(favIntent);
+                break;
+        }
+
+         */
+
+        return true;
+    }
 }
